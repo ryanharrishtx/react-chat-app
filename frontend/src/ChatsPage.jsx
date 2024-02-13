fef23dfa45dc252bf4baf87f41ad581a29902503
ryanharrishtx@gmail.com <ryanharrishtx@gmail.com>
2024-02-12 20:01:53 -0600

    chat functionality working

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,20 @@
-const ChatsPage = () => {
-    return <>Chats...</>
-}
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "c2949c24-57af-4248-bb0a-0c21c68585c1",
+    props.user.username,
+    props.user.secret
+  );
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
+};
 
-export default ChatsPage
+export default ChatsPage;
