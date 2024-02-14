6ac5951abf31ce98011a3ff6a35eeddac8045667
ryanharrishtx@gmail.com <ryanharrishtx@gmail.com>
2024-02-13 18:42:17 -0600

    heroku

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,10 @@ app.post("/authenticate", async (req, res) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
     });
 }
 
