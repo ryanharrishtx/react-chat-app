9d9b76f9e308ca0ca03426758462440371990f50
ryanharrishtx@gmail.com <ryanharrishtx@gmail.com>
2024-02-13 18:47:58 -0600

    heroku

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+    port: 3000, // Change to your desired port
+    proxy: {
+      '/api': { target: 'http://localhost:3001', secure: false, changeOrigin: true },
+    },
+  },
   build: {
     outDir: 'dist', // Output directory for the build
     assetsDir: '.', // Directory under outDir to place assets
