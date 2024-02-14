ffcf9a434af1caec4314781e35ad66c67ab43f74
ryanharrishtx@gmail.com <ryanharrishtx@gmail.com>
2024-02-13 20:08:42 -0600

    Heroku

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.post("/authenticate", async (req, res) => {
     const { username } = req.body;
